b501bf1b9a95923425b3f89f9578f94ac9dc1aed
Suhaib Saeed <saeed.suhaib@googlemail.com>
2020-11-28 13:05:07 -0800

    Added the rest of completed the exercises

---
+++ Week12/test_ex3/test_simple.py
@@ -0,0 +1,14 @@
+def my_add(x, y):
+    return x + y
+
+def my_mul(x, y):
+    return x * y
+
+# Test function for my_add func
+def test_my_add():
+    assert my_add(1, 2) == 3
+
+# Test function for my_mul func
+def test_my_mul():
+    assert my_mul(2, 2) == 4
+
